ba70a606c245c63f5ea02b61e2938c80b6a8a2dd
Niqee <niqz99@gmail.com>
2019-11-12 15:40:48 +0200

    Updated database adapter
    - Implemented save_to_table
    
    Signed-off-by: Niqee <niqz99@gmail.com>

--- database/util.py
+++ database/util.py
@@ -1,4 +1,6 @@
 import pyodbc
+import pandas as pd
+import warnings
 
 
 class DBAdapter(object):
@@ -34,5 +36,44 @@ class DBAdapter(object):
         main_command = main_blank.format(name=table_name, columns=columns_part)
         self.exec(main_command, no_return=True)
 
-    def save_to_table(self, data_df):
-        pass
+    def save_to_table(self,
+                      table_name: str,
+                      data_df: pd.DataFrame,
+                      use_into=False,
+                      create_table_params=None):
+        command_blank = "INSERT {into} {table_name} {columns} VALUES ({values})"
+
+        column_names = data_df.columns
+
+        if not self.cursor.tables(table=table_name, tableType='TABLE').fetchone():
+            self.create_table(table_name, create_table_params)
+
+        for idx in data_df.index:
+            values_str = ""
+            columns_str = ""
+            for column_name in column_names:
+                value = data_df.loc[idx, column_name]
+                if pd.notna(value):
+                    if type(value) == str:
+                        value = value.replace("'", "''")
+                        value = "'" + value + "'"
+                    values_str = "{prev}{value}, ".format(prev=values_str, value=value)
+                    columns_str = "{prev}{name}, ".format(prev=columns_str, name=column_name)
+            values_str = values_str[:-2]
+            columns_str = columns_str[:-2]
+            if use_into:
+                columns_str = '(' + columns_str + ')'
+                command = command_blank.format(into='INTO',
+                                               table_name=table_name,
+                                               columns=columns_str,
+                                               values=values_str)
+            else:
+                command = command_blank.format(into='',
+                                               table_name=table_name,
+                                               columns='',
+                                               values=values_str)
+            print(command)
+            try:
+                self.exec(command, no_return=True)
+            except pyodbc.IntegrityError:
+                warnings.warn("Cant exec command : '{}'".format(command))
