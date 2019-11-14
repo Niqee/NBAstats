5c09ae67d8767f56971bc953441309d8ad738e19
Niqee <niqz99@gmail.com>
2019-11-14 16:46:36 +0200

    Updated database adapter
    - Implemented select_from_table method
    - Implemented table_not_exists method
    - Implemented table_exists method
    - Added verbose functionality
    
    Signed-off-by: Niqee <niqz99@gmail.com>

--- database/util.py
+++ database/util.py
@@ -10,6 +10,7 @@ class DBAdapter(object):
                  server="DESKTOP-7L5ELCB",
                  database="nba_stats",
                  trusted_connection="yes"):
+
         settings_blank = 'Driver={};Server={};Database={};Trusted_Connection={};'
         conn = pyodbc.connect(settings_blank.format(driver,
                                                     server,
@@ -18,15 +19,28 @@ class DBAdapter(object):
                               autocommit=True)
         self.cursor = conn.cursor()
 
-    def exec(self, query: str, no_return: bool = False):
+    def exec(self,
+             query: str,
+             no_return: bool = False,
+             verbose: bool = True):
+
+        if verbose:
+            print(query)
         self.cursor.execute(query)
         if not no_return:
             return list(self.cursor)
 
-    def create_table(self, table_name: str, params: list):
-        main_blank = "CREATE TABLE {name} ({columns});"
+    def create_table(self,
+                     table_name: str,
+                     params: list,
+                     verbose: bool = True):
+
+        main_blank = "CREATE TABLE [{name}] ({columns});"
         column_blank = "{name} {type} {attrs}, "
         columns_part = ""
+
+        if self.table_exists(table_name):
+            raise Exception('Table "{}" is already exists'.format(table_name))
 
         table_columns_params = [{"name": name, "type": v_type, "attrs": attrs} for name, v_type, attrs in params]
 
@@ -34,18 +48,21 @@ class DBAdapter(object):
             t_column = column_blank.format(**t_column_params)
             columns_part += t_column
         main_command = main_blank.format(name=table_name, columns=columns_part)
-        self.exec(main_command, no_return=True)
+
+        self.exec(main_command, no_return=True, verbose=verbose)
 
     def save_to_table(self,
                       table_name: str,
                       data_df: pd.DataFrame,
                       use_into=False,
-                      create_table_params=None):
-        command_blank = "INSERT {into} {table_name} {columns} VALUES ({values})"
+                      create_table_params=None,
+                      verbose=True):
+
+        command_blank = "INSERT {into} [{table_name}] {columns} VALUES ({values})"
 
         column_names = data_df.columns
 
-        if not self.cursor.tables(table=table_name, tableType='TABLE').fetchone():
+        if self.table_not_exists(table_name):
             self.create_table(table_name, create_table_params)
 
         for idx in data_df.index:
@@ -72,8 +89,37 @@ class DBAdapter(object):
                                                table_name=table_name,
                                                columns='',
                                                values=values_str)
-            print(command)
             try:
-                self.exec(command, no_return=True)
+                self.exec(command, no_return=True, verbose=verbose)
             except pyodbc.IntegrityError:
                 warnings.warn("Cant exec command : '{}'".format(command))
+
+    def select_from_table(self,
+                          table_name: str,
+                          verbose: bool = True,
+                          **params):
+
+        command_blank = 'SELECT {columns} FROM [{table}] {where} {condition}'
+        if ('columns' not in params.keys()) or (not params['columns']):
+            params['columns'] = '*'
+
+        if self.table_not_exists(table_name):
+            raise Exception('Can`t find table with name "{name}"'.format(name=table_name))
+
+        if ('condition' in params.keys()) and (params['condition']):
+            command = command_blank.format(columns=params['columns'],
+                                           table=table_name,
+                                           where='WHERE',
+                                           condition=params['condition'])
+        else:
+            command = command_blank.format(columns=params['columns'],
+                                           table=table_name,
+                                           where='',
+                                           condition='')
+        return self.exec(command, verbose=verbose)
+
+    def table_not_exists(self, table_name: str):
+        return not self.exec("SELECT * FROM information_schema.tables WHERE table_name = '{}'".format(table_name))
+
+    def table_exists(self, table_name: str):
+        return not self.table_not_exists(table_name)
