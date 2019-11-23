da1fb6d6371552db478163a5538e3552aa59dd6c
Niqee <niqz99@gmail.com>
2019-11-23 23:14:33 +0200

    Updated Logger
    - Added cleaning old logs
    - Fixed log_error
    
    Signed-off-by: Niqee <niqz99@gmail.com>

--- logger/util.py
+++ logger/util.py
@@ -1,13 +1,21 @@
 from datetime import datetime
+import os
+import shutil
 
 
 # TODO: Add clean logs methods
 class Logger(object):
-    def __init__(self, logs_dir_path: str = "logger/logs"):
+    def __init__(self, logs_dir_path: str = "logger/logs", clean_old_logs=False):
         self.log_path_blank = logs_dir_path + r"/{}"
 
         self.log_success_path = self.log_path_blank.format("log_ok.txt")
         self.log_error_path = self.log_path_blank.format("log_err.txt")
+
+        if clean_old_logs and os.path.isdir(logs_dir_path):
+            shutil.rmtree(logs_dir_path)
+
+        if not os.path.isdir(logs_dir_path):
+            os.mkdir(logs_dir_path)
 
     def log_error(self, url, error_msg):
         current_time = datetime.now()
@@ -15,7 +23,7 @@ class Logger(object):
             log_err_file.write("{time}|{timestamp}|{url}|{msg}\n".format(time=current_time,
                                                                          timestamp=current_time.timestamp(),
                                                                          url=url,
-                                                                         msg=error_msg))
+                                                                         msg=error_msg.replace('\n', '; ')))
 
     def log_success(self, url):
         current_time = datetime.now()
